8fa12bf41df5a4306a260de479bc90fc890a7be9
Connor Holly <connorholly11@gmail.com>
2019-09-13 11:31:48 -0400

    putting in the actions into reducer

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,10 +1,15 @@
 import axios from 'axios';
 
-const FETCHING_SMURF_START = 'FETCHING_SMURF_START';
-const FETCHING_SMURF_SUCCESS = 'FETCHING_SMURF_SUCCESS';
-const FETCHING_SMURF_FAILURE = 'FETCHING_SMURF_FAILURE';
+export const FETCHING_SMURF_START = 'FETCHING_SMURF_START';
+export const FETCHING_SMURF_SUCCESS = 'FETCHING_SMURF_SUCCESS';
+export const FETCHING_SMURF_FAILURE = 'FETCHING_SMURF_FAILURE';
 
 export const getSmurf = () => dispatch => {
     dispatch({FETCHING_SMURF_START})
 
+    axios.get('http://localhost:3333/smurfs')
+        .then(response => {
+            console.log(response)
+            dispatch({FETCHING_SMURF_SUCCESS})
+        })
 }
--- smurfs/src/reducers/SmurfReducer.js
+++ smurfs/src/reducers/SmurfReducer.js
@@ -1,4 +1,4 @@
-
+import {FETCHING_SMURF_START, FETCHING_SMURF_SUCCESS, FETCHING_SMURF_FAILURE} from '../actions/index';
 
 export const initialState = {
     smurfs: [],
@@ -9,6 +9,24 @@ export const initialState = {
 
 export const SmurfReducer = (state = initialState, action) => {   
     switch(action.type){
+        case FETCHING_SMURF_START: 
+            return {
+                ...state,
+                isFetching: true,
+                error: ''
+            }
+        case FETCHING_SMURF_SUCCESS:
+            return{
+                ...state,
+                smurfs: action.payload,
+                isFetching: false,
+                error: ''
+            }
+        case FETCHING_SMURF_FAILURE:
+            return{
+                ...state,
+                error: action.payload
+            }
 
         default:
             return state;
