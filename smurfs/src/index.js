a6d35ce827afd9d29b72dc0e175cfd0163c66a0c
Connor Holly <connorholly11@gmail.com>
2019-09-13 11:17:08 -0400

    set up store and provider, set up inital state and mock reducer, need to get info from API

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -3,4 +3,18 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import {createStore, applyMiddleware, compose} from 'redux';
+import {Provider} from 'react-redux';
+import thunk from 'redux-thunk';
+import {SmurfReducer} from './reducers/SmurfReducer';
+
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(SmurfReducer,composeEnhancers(applyMiddleware(thunk)));
+
+ReactDOM.render(
+<Provider store={store}>
+    <App /> 
+</Provider>,
+document.getElementById('root'));
---
+++ smurfs/src/reducers/SmurfReducer.js
@@ -0,0 +1,16 @@
+
+
+export const initialState = {
+    smurfs: [],
+    isFetching: false,
+    error: ''
+}
+
+
+export const SmurfReducer = (state = initialState, action) => {   
+    switch(action.type){
+
+        default:
+            return state;
+    }
+}
